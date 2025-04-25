7dd870fb76e09c773c2aea96c02d193bc2dd5a31
hry-missannil <missannil@163.com>
2025-04-25 17:45:56 +0800

    feat: chunkTag 内部的text 可以使用 RootComponent中定义的变量

--- src/wxmlChecker/tools/getLegalVariables.ts
+++ src/wxmlChecker/tools/getLegalVariables.ts
@@ -1,16 +1,22 @@
 import type { CheckContext } from "../CheckContext";
 
 export function getLegalVariables(checkContext: CheckContext): string[] {
-  const { tsFileInfo, wxForInfos, outerChunkTagMasks } = checkContext;
-  if (outerChunkTagMasks.length > 0) {
-    return checkContext.getOuterChunkTagVariables().concat(
-      wxForInfos.itemNames,
-      wxForInfos.indexNames,
-    );
-  } else {
-    return tsFileInfo.rootComponentInfo.dataList.concat(
-      wxForInfos.itemNames,
-      wxForInfos.indexNames,
-    );
-  }
+  const { tsFileInfo, wxForInfos } = checkContext;
+
+  return tsFileInfo.rootComponentInfo.dataList.concat(
+    wxForInfos.itemNames,
+    wxForInfos.indexNames,
+    checkContext.getOuterChunkTagVariables(),
+  );
+  // if (outerChunkTagMasks.length > 0) {
+  //   return checkContext.getOuterChunkTagVariables().concat(
+  //     wxForInfos.itemNames,
+  //     wxForInfos.indexNames,
+  //   );
+  // } else {
+  //   return tsFileInfo.rootComponentInfo.dataList.concat(
+  //     wxForInfos.itemNames,
+  //     wxForInfos.indexNames,
+  //   );
+  // }
 }
